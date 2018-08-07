f7d0eacbc13f452088e38dbe1fec25aa44c788d1
Matthew Ralph <ralpmat@gmail.com>
2018-08-07 17:41:01 +0100

    test graph-creation of model training history for report

--- keras_character_based_ner/src/matt/history.py
+++ keras_character_based_ner/src/matt/history.py
@@ -9,6 +9,16 @@ from typing import Dict
 
 
 def graph_model_history(filepath, dest_file_name):
+    """
+    Open a pickled `history` object created by a train (fit() invocation),
+    and graph out the non-null-label accuracy, categorical accuracy, and loss
+    on both training and validation datasets.
+    :param filepath: path to the pickled history file.
+    :param dest_file_name: a destination file name for the files. This name will
+    be used 3 times, with words added to indicate which metric is shown in its graph.
+    e.g. 'toy-model'
+    :return:
+    """
     import matplotlib
     matplotlib.use('TkAgg')
     import matplotlib.pyplot as plt
@@ -23,16 +33,31 @@ def graph_model_history(filepath, dest_file_name):
 
     epochs = range(1, len(cat_acc) + 1)
 
+    plt.figure(1)
+
     plt.plot(epochs, cat_acc, 'bo', label='Training acc')
     plt.plot(epochs, val_cat_acc, 'b', label='Validation acc')
     plt.title('Training and validation accuracy')
     plt.legend()
 
-    plt.figure()
+    plt.savefig('keras_character_based_ner/graphs/{}-acc.png'.format(dest_file_name))
+
+    print("BLAH")
+
+    plt.figure(2)
 
     plt.plot(epochs, loss, 'bo', label='Training loss')
     plt.plot(epochs, val_loss, 'b', label='Validation loss')
     plt.title('Training and validation loss')
     plt.legend()
 
-    plt.savefig('keras_character_based_ner/src/{}'.format(dest_file_name))
+    plt.savefig('keras_character_based_ner/graphs/{}-loss.png'.format(dest_file_name))
+
+    plt.figure(3)
+
+    plt.plot(epochs, non_null_label_acc, 'bo', label='Non null label accuracy')
+    plt.plot(epochs, val_non_null_label_acc, 'b', label='Validation Non null label accuracy')
+    plt.title('Training and validation non null label accuracy')
+    plt.legend()
+
+    plt.savefig('keras_character_based_ner/graphs/{}-non-null-label-acc.png'.format(dest_file_name))
