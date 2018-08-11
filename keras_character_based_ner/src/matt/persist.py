19f1f4d7e85bc9e1798edf04ca720186c7fcecb0
Matthew Ralph <ralpmat@gmail.com>
2018-08-11 18:39:49 +0100

    Refactor to fix circular imports

---
+++ keras_character_based_ner/src/matt/persist.py
@@ -0,0 +1,44 @@
+from keras_character_based_ner.src.model import CharacterBasedLSTMModel
+from typing import Callable, Dict
+from keras.models import load_model, Sequential  # type: ignore
+
+
+class SavedCharacterBasedLSTMModel(CharacterBasedLSTMModel):
+    def __init__(self, config, dataset):
+        super().__init__(config, dataset)
+
+    def save(self, filepath):
+        """
+        MIR Added method to save model to disk
+        :param filepath: file path under which to save
+        :return:
+        """
+        return self.model.save(filepath)
+
+
+class LoadedToyModel(SavedCharacterBasedLSTMModel):
+    """
+    A loaded model with all the functionality of a CharacterBasedLSTMModel
+    """
+    def get_model(self):
+        print("Loading in model from previous training of Toy dataset")
+        model_path = "keras_character_based_ner/src/toy_dataset.keras.h5"
+        custom_objects: Dict[str, Callable] = {
+            'non_null_label_accuracy': SavedCharacterBasedLSTMModel.non_null_label_accuracy
+        }
+        model: Sequential = load_model(model_path, custom_objects=custom_objects)
+        print("Completed loading in model from previous training of Toy dataset")
+        return model
+
+
+class LoadedMiniModel(SavedCharacterBasedLSTMModel):
+    """
+    A loaded model with all the functionality of a CharacterBasedLSTMModel
+    """
+    def get_model(self):
+        print("Loading in model from previous training of Mini dataset")
+        model_path = "keras_character_based_ner/src/mini_dataset.keras.h5"
+        custom_objects: Dict[str, Callable] = {
+            'non_null_label_accuracy': SavedCharacterBasedLSTMModel.non_null_label_accuracy
+        }
+        self.model: Sequential = load_model(model_path, custom_objects=custom_objects)
--- keras_character_based_ner/src/matt/predict.py
+++ keras_character_based_ner/src/matt/predict.py
@@ -1,34 +1,6 @@
-from keras.models import load_model, Sequential  # type: ignore
-from keras_character_based_ner.src.matt.train import SavedCharacterBasedLSTMModel
-from typing import Callable, Dict
 from keras_character_based_ner.src.config import Config
+from keras_character_based_ner.src.matt.persist import LoadedToyModel, LoadedMiniModel
 from keras_character_based_ner.src.dataset import CharBasedNERDataset
-
-
-class LoadedToyModel(SavedCharacterBasedLSTMModel):
-    """
-    A loaded model with all the functionality of a CharacterBasedLSTMModel
-    """
-    def get_model(self):
-        print("Loading in model from previous training of Toy dataset")
-        model_path = "keras_character_based_ner/src/toy_dataset.keras.h5"
-        custom_objects: Dict[str, Callable] = {
-            'non_null_label_accuracy': SavedCharacterBasedLSTMModel.non_null_label_accuracy
-        }
-        self.model: Sequential = load_model(model_path, custom_objects=custom_objects)
-
-
-class LoadedMiniModel(SavedCharacterBasedLSTMModel):
-    """
-    A loaded model with all the functionality of a CharacterBasedLSTMModel
-    """
-    def get_model(self):
-        print("Loading in model from previous training of Mini dataset")
-        model_path = "keras_character_based_ner/src/mini_dataset.keras.h5"
-        custom_objects: Dict[str, Callable] = {
-            'non_null_label_accuracy': SavedCharacterBasedLSTMModel.non_null_label_accuracy
-        }
-        self.model: Sequential = load_model(model_path, custom_objects=custom_objects)
 
 
 def model_toy_predict_file(file_path: str):
