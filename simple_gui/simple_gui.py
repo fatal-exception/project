71d58bc5f09feb673d72ff92a1be77cd888571c8
Matthew Ralph <ralpmat@gmail.com>
2018-08-18 11:27:26 +0100

    Attempt first endpoint to predict text

--- simple_gui/simple_gui.py
+++ simple_gui/simple_gui.py
@@ -1,8 +1,16 @@
 from typing import List, Tuple
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from hansard_gathering import filesystem
+from keras_character_based_ner.src.matt.persist import LoadedToyModel
+from keras_character_based_ner.src.matt.eval import init_config_dataset
 
 app = Flask(__name__)
+
+cache = {}
+
+
+def initialize_keras_model():
+    cache["model"] = LoadedToyModel(*init_config_dataset())
 
 
 @app.route('/')
@@ -11,25 +19,29 @@ def get_dates_list():
     return render_template('index.html', dates=dates)
 
 
-@app.route('/<date>/')
+@app.route('/date/<date>/')
 def get_hansards_by_date(date):
     debates: List[Tuple[int, str]] = list(filesystem.get_debates_by_date(date))
     return render_template('date.html', date=date, debates=debates)
 
 
-@app.route('/<date>/<debate_title>')
+@app.route('/date/<date>/<debate_title>')
 def view_hansard(date, debate_title):
-    pass
     debate = filesystem.view_hansard(date, debate_title)
     debate_paras = debate.split("\n")
     return render_template('debate.html', date=date, debate_title=debate_title, debate_paras=debate_paras)
 
 
 # Add a 'predict' route for AJAX posting of content to be predicted
-@app.route('/predict')
-def predict_text(text):
-    pass
+@app.route('/predict/', methods=['POST'])
+def predict_text():
+    model = cache['model']
+    data = str(request.get_data())
+    print(data)
+    prediction = model.predict_long_str(data)
+    return prediction
 
 
 def main():
+    initialize_keras_model()
     app.run(load_dotenv=False, debug=True, port=5000)
