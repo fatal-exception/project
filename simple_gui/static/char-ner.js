b63588e2fd9a9aa67f76e2f841ac871a760140d2
Matthew Ralph <ralpmat@gmail.com>
2018-09-07 13:00:57 +0100

    Set up JS for remote hosting

--- simple_gui/static/char-ner.js
+++ simple_gui/static/char-ner.js
@@ -3,7 +3,7 @@ $( ".unrendered" ).click(function(event) {
     const $elem = $( this );
     const text = $elem.text();
     $elem.effect("bounce", "slow");
-    $.post("http://localhost:5000/predict/", text, function(data) {
+    $.post("http://193.61.29.202:5000/predict/", text, function(data) {
         $elem.text(data);
         $elem.removeClass("unrendered");
         $elem.addClass("rendered");
